1bfd9f1932692581d93571632a56560f4a90729a
7905265903rajat <rajat0323@gmail.com>
2026-02-15 19:25:31 +0530

    Fix sitemap for dynamic blog slugs

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 
+const SITE_URL = 'https://symptomsinsight.com'
+
 export async function getServerSideProps({ res }) {
   const postsDirectory = path.join(process.cwd(), 'posts')
   const years = fs.readdirSync(postsDirectory)
@@ -14,24 +16,23 @@ export async function getServerSideProps({ res }) {
     const files = fs.readdirSync(yearPath)
     files.forEach((file) => {
       if (file.endsWith('.md')) {
-        const slug = file.replace('.md', '')
-        urls.push(
-          `<url>
-            <loc>https://symptomsinsight.com/blog/${slug}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-          </url>`
-        )
+        const slug = file.replace(/\.md$/, '')
+        urls.push(`
+  <url>
+    <loc>${SITE_URL}/blog/${slug}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </url>`)
       }
     })
   })
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://symptomsinsight.com/</loc>
-    </url>
-    ${urls.join('')}
-  </urlset>`
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${SITE_URL}/</loc>
+  </url>
+  ${urls.join('')}
+</urlset>`
 
   res.setHeader('Content-Type', 'text/xml')
   res.write(sitemap)
@@ -40,6 +41,9 @@ export async function getServerSideProps({ res }) {
   return { props: {} }
 }
 
+/**
+ * ✅ Required dummy export for Next.js
+ */
 export default function Sitemap() {
   return null
 }
