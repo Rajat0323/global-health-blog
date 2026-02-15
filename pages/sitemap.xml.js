f5d2df18b2ac108ca69b882b508856d60abaf513
7905265903rajat <rajat0323@gmail.com>
2026-02-15 19:21:08 +0530

    Fix sitemap build issue (Next.js compliant)

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,13 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 
-const SITE_URL = 'https://symptomsinsight.com' // change if needed
-
 export async function getServerSideProps({ res }) {
   const postsDirectory = path.join(process.cwd(), 'posts')
   const years = fs.readdirSync(postsDirectory)
 
-  let urls = []
+  const urls = []
 
   years.forEach((year) => {
     const yearPath = path.join(postsDirectory, year)
@@ -16,37 +14,32 @@ export async function getServerSideProps({ res }) {
     const files = fs.readdirSync(yearPath)
     files.forEach((file) => {
       if (file.endsWith('.md')) {
-        const slug = file.replace(/\.md$/, '')
-        urls.push(`${SITE_URL}/blog/${slug}`)
+        const slug = file.replace('.md', '')
+        urls.push(
+          `<url>
+            <loc>https://symptomsinsight.com/blog/${slug}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+          </url>`
+        )
       }
     })
   })
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${SITE_URL}</loc>
-  </url>
-  ${urls
-    .map(
-      (url) => `
-  <url>
-    <loc>${url}</loc>
-  </url>`
-    )
-    .join('')}
-</urlset>`
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://symptomsinsight.com/</loc>
+    </url>
+    ${urls.join('')}
+  </urlset>`
 
   res.setHeader('Content-Type', 'text/xml')
   res.write(sitemap)
   res.end()
 
-  return {
-    props: {},
-  }
+  return { props: {} }
 }
 
-// ⚠️ REQUIRED dummy export (VERY IMPORTANT)
 export default function Sitemap() {
   return null
 }
